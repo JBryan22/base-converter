bfcd7df198dbf674416ac19ebc1c23d396a549b9
Jesse Bryan and Victor Puentes <jesse@>
2017-07-12 16:04:27 -0700

    add functionality for bases above 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
+var hexNums = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+
 var baseConverter = function(number, base) {
   var newNumArr = [];
-  while (number >= 1) {
-    newNumArr.push(number % base);
-    number = parseInt(number / base);
+  if (base > 10) {
+    while (number >= 1) {
+      newNumArr.push(hexNums[(number % base)])
+      number = parseInt(number / base);
+    }
+    return newNumArr.reverse().join('');
+  } else {
+    while (number >= 1) {
+      newNumArr.push(number % base);
+      number = parseInt(number / base);
+    }
+    return newNumArr.reverse().join('');
   }
-  return newNumArr.reverse().join('');
 }
 
 
